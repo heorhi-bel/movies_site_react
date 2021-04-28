5da4e22569df24646f2bc6ffcfb225af57cfce15
heorhi-bel <georgix@mail.ru>
2021-04-28 12:02:18 +0200

    some changes with styles

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -42,7 +42,7 @@ class Search extends React.Component {
                             this.props.searchMovies(this.state.search)
                         }
                     >
-                        SEARCH!
+                        SEARCH
                     </button>
 
                     <div className="filters">
